ab85c468c69b432076119140fb25e6cb2684ab3a
aliianza12 <juanv9664@gmail.com>
2020-06-03 19:07:21 -0500

    update controller product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Product;
-use App\category;
+use App\Category;
 
 class ProductController extends Controller
 {
